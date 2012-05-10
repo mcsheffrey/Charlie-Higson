7114b7e712125c8ae87dec209dee1040ef538dbc
Connor McSheffrey <connor.mcsheffrey@gmail.com>
2012-05-10 09:33:35 -0700

    responsive and shiz

--- js/script.js
+++ js/script.js
@@ -11,56 +11,65 @@
     
   }
 
-  $('#introduction').parallax(0, 2500, 0.1, true);
+  // $('#introduction').parallax(0, 2500, 0.1, true);
   $('#photos').parallax(0, 1500, 0.1, true);
   $('#additional-details').parallax(0, 500, 0.1, true);
 
-  // Chris Coyier Sublime Video Trick
+  // Sublime Video Responsive Player
 
-  // Prevents some flickering
-  $('#the-video').css("visibility", "hidden");
+  // media query event handler
+  if (matchMedia) {
+    var mq = window.matchMedia("(max-width: 1000px)");
+    mq.addListener(WidthChange);
+    WidthChange(mq);
+  }
+  // media query change
+  function WidthChange(mq) {
+    if (mq.matches) {
+      $(window).resize(function(){
+        sublimeResize();
+      });
+    }
+  }
 
-  // Fluid column video is inside of
-  var fluidParent = $(".main-column"),
-  newWidth, newHeight;
-
-  // Gets called when video needs resizing
-  function resizeVideo() {
-    newWidth = fluidParent.width();
-    // 1.78125 == Aspect Ratio of my videos
-    sublimevideo.resize('the-video', newWidth, newWidth/1.78125);
-  };
-
-  $(window).on('resize', function(event) {
-    console.log(this);
-    
-    resizeVideo();
-  });
-
-  // When the resources are ready,
-  // load up the video and size it correctly
-  sublimevideo.ready(function() {
-    sublimevideo.prepare('the-video');
-    resizeVideo();
-  });
-
+function sublimeResize(){
+  var sublimeWidth=$(".video-container").innerWidth();
+  /*
+    Set sublimeAspect according to the aspect ratio of your video.
+    Widescreen (16:9) = 1.7777778
+    Standard (4:3) = 1.3333333
+    Determine your own aspect ratio by width/height.
+  */
+  var sublimeAspect = 1.8049327;
+  sublimevideo.resize("rejecting-the-noise",sublimeWidth,sublimeWidth/sublimeAspect);
+}
   // ScrollTo Nav links
 
   $('#navbar a').on('click', function(event) {
     event.preventDefault();
 
     target = this.hash;
-    $.scrollTo(target, 500);
+    $.scrollTo(target, 500, {easing:'easeInOutQuint'});
   });
 
   $('#navbar').scrollspy();
 
+  $('#photos').flexslider({
+    animation: "slide",
+    controlNav: true,
+    controlsContainer: "#pagi",
+    directionNav: false,
+    slideshow: false,
+    keyboardNav: true,
+    animationLoop: false
+  });
+
   $(window).on('scroll', function(event) {
     var scrolled = $(window).scrollTop();
-    $('#parallax-1').css('top',(-100+(scrolled*0.5))+'px');
-    $('#parallax-2').css('top',(-100+(scrolled*0.3))+'px');
-    $('#parallax-3').css('top',(-200+(scrolled*0.4))+'px');
-    $('#parallax-4').css('top',(200+(scrolled*0.6))+'px');
+    $('#parallax-1').css('top',(-100+(scrolled*0.1))+'px');
+    $('#parallax-2').css('top',(300+(scrolled*0.2))+'px');
+    $('#parallax-3').css('top',(-200+(scrolled*0.2))+'px');
+    $('#parallax-4').css('top',(300+(scrolled*0.1))+'px');
   });
 
 }());
